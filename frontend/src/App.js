9368ead7a0dc39eb6b21d6265eb6beb40c82a70d
Antonio Santana <antcesar.santana@gmail.com>
2021-11-21 17:59:32 -0300

    Category

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,24 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./screens/home";
+import Category from "./screens/category";
 import Login from "./screens/login";
-import './stylesheets/App.css';
+import "./stylesheets/App.css";
 
 function App() {
-  const [token, setToken] = useState(1)
-  
-  const LoginRoute = () => (
-    <Login />
-  )
+  const [token, setToken] = useState(1);
+
+  const LoginRoute = () => <Login />;
 
   const TasksRoute = () => (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/category" element={<Category />} />
       {/* <Route path="about" element={<About />} /> */}
     </Routes>
-  )
-
-  return (
-    <div className="App">
-      {token ? TasksRoute() : LoginRoute()}
-    </div>
   );
-} 
+
+  return <div className="App">{token ? TasksRoute() : LoginRoute()}</div>;
+}
 
 export default App;
